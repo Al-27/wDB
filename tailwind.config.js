0727df4088dcfe8b6a759df3da5889252ae2cc42
Al-27 <125827852+Al-27@users.noreply.github.com>
2025-01-01 21:46:45 +0000

    extending tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./flask_app/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns:{
+        'wdb2' : 'minmax(4rem, 12rem) minmax(75%,100%)'   
+      },
+      gridTemplateRows:{
+        'wdb2' : 'minmax(4rem, 6rem) 75%;'  
+      },
+      boxShadow:{
+        //innerSmall
+        'is': '0 0 1px 1px rgba(0,0,0,.3)' 
+      },
+      height:{
+        '21': '5.25rem',
+        '22': '5.5rem',
+        '23': '5.75rem'
+      },
+      lineHeight:{
+        '2': '.5rem'
+      }
+    },
   },
   plugins: [],
 }
