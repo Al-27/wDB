5d9db174cab1273a7a896561d6c8a4ee6f85b033
Al-27 <125827852+Al-27@users.noreply.github.com>
2025-01-14 04:49:23 +0000

    upate files

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from api import api_b
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(api_b)
+
 
 @app.route('/')
 def index_r():
     return render_template('main.html')
+
