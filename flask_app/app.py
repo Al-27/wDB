04117c612dca611be22ccb4d8e87e777502443b1
Al-27 <125827852+Al-27@users.noreply.github.com>
2025-01-23 00:37:22 +0000

    retrieve the requested modal

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from api import api_b
 
-app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(api_b)
 
+app = Flask(__name__) 
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(api_b) 
 
 @app.route('/')
 def index_r():
     return render_template('main.html')
 
+@app.route('/m/<modal>')
+def modal_r(modal):
+    return render_template(f'{modal}.html')
