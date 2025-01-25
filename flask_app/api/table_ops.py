02d6fdbd8b8a4a5662c6164197ce092df0ad7ce6
Al-27 <125827852+Al-27@users.noreply.github.com>
2025-01-25 07:54:43 +0000

    add  delete route for dropping tables

--- flask_app/api/table_ops.py
+++ flask_app/api/table_ops.py
@@ -1,8 +1,7 @@
 from flask import make_response, abort, jsonify, request
 from . import api_b
 from db import db_e
-from db.utils import to_obj, rows_to_list
-from flask import Blueprint
+from db.utils import to_obj, rows_to_list 
 
 @api_b.route('/tables',methods=['GET'])
 def get_tables():
@@ -13,8 +12,28 @@ def select_table(table_name):
     rows = db_e.switch_to_table(table_name)
     return jsonify(rows_to_list(rows))
 
+@api_b.route('/table', methods=['POST'],strict_slashes=False)
 @api_b.route('/table/<data>', methods=['POST'],strict_slashes=False)
-def add_table(data):
-    obj = to_obj(data)
-    db_e.new_table(**obj)
+def add_table(data=None):
+    obj = data
+    if obj is not None:
+        obj = to_obj(data)
+    else:
+        obj = request.json
+        print(request.json)
+        print(request.data)
+    try:
+        db_e.new_table(**obj)
+    except Exception as e:
+        return {'error': str(e)},401
+    return jsonify({'res':'ok'})
+
+@api_b.route('/table', methods=['DELETE'],strict_slashes=False, defaults={'table_name':None})
+@api_b.route('/table/<table_name>', methods=['DELETE'],strict_slashes=False)
+def drop_table(table_name):
+    try:
+        print(table_name)
+        db_e.drop_table(table_name) 
+    except Exception as e:
+        return {'error': str(e)},401
     return jsonify({'res':'ok'})
